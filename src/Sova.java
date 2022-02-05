e2c558efa70b4fd0f30822701d28b65e43056e5c
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2022-02-05 13:39:16 -0500

    removed dummy test class

--- src/Sova.java
+++
@@ -1,9 +0,0 @@
-import Employees.Check;
-
-public class Sova {
-    
-    public static void main(String[] args) {
-        Check testCheck = new Check("Freedom Convoy", 420.69);
-        System.out.println(testCheck);
-    }
-}
